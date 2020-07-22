3b622226ceebe92e76b5578b5026d2a1a1e2597c
Lei Yu <yuleiz@gmail.com>
2020-07-22 14:51:44 +0800

    add keyboard directory

---
+++ keyboards/m20add/m20add.h
@@ -0,0 +1,13 @@
+/**
+ * m20add.h
+ * common including files
+ */
+
+#pragma once
+
+#include "config.h"
+#include "keycode.h"
+#include "action.h"
+#include "action_macro.h"
+#include "action_code.h"
+#include "keymap.h"
