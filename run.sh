4ccf2b343593ae95724709608e12a5d982d75121
Lei Yu <yuleiz@gmail.com>
2020-07-29 17:57:56 +0800

    cleanup cmake files

--- run.sh
+++ run.sh
@@ -1,29 +1,20 @@
 #! /bin/sh
 
-if [ $# -lt 2 ]; then
-    echo "usage: run.sh env mcu"
+if [ $# -lt 1 ]; then
+    echo "usage: run.sh mcu"
 else
-    if [ $1 == "home" ]; then
-        tp="/home/astro/arm/gcc-arm-none-eabi-9-2019-q4-major-win32"
-    elif [ $1 == "work" ]; then
-        tp="/home/leiyu/qmk_utils/gcc-arm-none-eabi"
-    else 
-        echo "Unknown enviroment, exiting..."
-        exit
-    fi
-
-    if [ $2 == "nrf52" ]; then
+    if [ $1 == "nrf52832" ]; then
         tf="./cmake/toolchain_nrf5x.cmake"
-        #mt="NRF52840"
-        #mc="NRF52840_XXAA"
         mt="NRF52832"
-        mc="NRF52832_XXAA"
-    elif [ $2 == "stm32f4" ]; then
+    elif [ $1 == "nrf52840" ]; then
+        tf="./cmake/toolchain_nrf5x.cmake"
+        mt="NRF52840"
+    elif [ $1 == "stm32f4" ]; then
         tf="./cmake/toolchain_stm32f4.cmake"
         mt="STM32F411"
     else
         echo "Unknown mcu, exiting..."
         exit
     fi
-    cmake -S . -B build -DMCU_TYPE=${mt} -DMCU_CHIP=${mc} -DCMAKE_TOOLCHAIN_FILE=${tf} -DTOOLCHAIN_PREFIX=${tp}
+    cmake -S . -B build -DMCU_TYPE=${mt} -DCMAKE_TOOLCHAIN_FILE=${tf}
 fi
