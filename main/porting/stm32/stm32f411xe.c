075cc7424001014846b869ad9827e14210d380b2
Lei Yu <yuleiz@gmail.com>
2020-11-09 23:09:12 +0800

    using 16m crystal

--- main/porting/stm32/stm32f411xe.c
+++ main/porting/stm32/stm32f411xe.c
@@ -40,7 +40,7 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLM = HSE_VALUE/1000000;
     RCC_OscInitStruct.PLL.PLLN = 384;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
     RCC_OscInitStruct.PLL.PLLQ = 8;
