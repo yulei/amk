a5dc45fccbc57c35d91073606199f0b11f989faa
Lei Yu <yuleiz@gmail.com>
2020-12-07 21:52:51 +0800

    fixed d port activation

--- main/porting/stm32/stm32f405xx.c
+++ main/porting/stm32/stm32f405xx.c
@@ -61,6 +61,7 @@ void SystemClock_Config(void)
 static void MX_GPIO_Init(void)
 {
     __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
