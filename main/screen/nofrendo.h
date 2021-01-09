372d30796fe8b2a6acc3c31e4f59cb11e9ac0216
Lei Yu <yuleiz@gmail.com>
2021-01-09 19:11:30 +0800

    added nofrendo driver files

---
+++ main/screen/nofrendo.h
@@ -0,0 +1,16 @@
+/**
+ * @file nofrendo.h
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void nofrendo_init(void);
+bool nofrendo_start(const char* rom);
+void nofrendo_task(uint32_t ticks);
+void nofrendo_pause(void);
+bool nofrendo_resume(void);
+void nofrendo_stop(void);
+void nofrendo_deinit(void);
