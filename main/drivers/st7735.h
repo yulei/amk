93e3b1cf782b48c9423c75112f51135181febec1
Lei Yu <yuleiz@gmail.com>
2021-02-23 23:42:46 +0800

    use st7735 for screen

---
+++ main/drivers/st7735.h
@@ -0,0 +1,25 @@
+
+/**
+ * @file st7735.h
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "gpio_pin.h"
+
+
+typedef struct {
+#ifdef SCREEN_POWER_PIN
+    pin_t power;
+#endif
+    pin_t reset;
+    pin_t cs;
+    pin_t dc;
+} st7735_t;
+
+void st7735_init(st7735_t *driver);
+void st7735_fill_rect(st7735_t *driver, uint32_t x, uint32_t y, uint32_t width, uint32_t height, const void *data, size_t size);
+void st7735_fill(st7735_t *driver, const void* data);
+void st7735_uninit(st7735_t *driver);
--- main/screen/screen.c
+++ main/screen/screen.c
@@ -4,10 +4,20 @@
 
 #include "screen.h"
 #include "gpio_pin.h"
-#include "ssd1357.h"
-//#include "lvgl.h"
 
-static ssd1357_t ssd1357_drivers[SCREEN_NUM] = {
+#ifndef SCREEN_DRIVER
+#define SCREEN_DRIVER_ST7735
+#endif
+
+#ifdef SCREEN_DRIVER_ST7735
+#include "st7735.h"
+#define screen_driver_t st7735_t
+#else
+#include "ssd1357.h"
+#define screen_driver_t ssd1357_t 
+#endif
+
+static screen_driver_t screen_drivers[SCREEN_NUM] = {
     {SCREEN_0_RESET, SCREEN_0_CS, SCREEN_0_DC},
 #ifdef SCREEN_1_PRESENT
     {SCREEN_1_RESET, SCREEN_1_CS, SCREEN_1_DC},
@@ -22,13 +32,17 @@ void screen_init(void)
     //lv_init();
 
     for (int i = 0; i < SCREEN_NUM; i++) {
-        gpio_write_pin(ssd1357_drivers[i].reset, 0);
-        gpio_write_pin(ssd1357_drivers[i].cs, 1);
-        gpio_write_pin(ssd1357_drivers[i].dc, 1);
-        gpio_set_output_pushpull(ssd1357_drivers[i].reset);
-        gpio_set_output_pushpull(ssd1357_drivers[i].cs);
-        gpio_set_output_pushpull(ssd1357_drivers[i].dc);
-        ssd1357_init(&ssd1357_drivers[i]);
+        gpio_write_pin(screen_drivers[i].reset, 0);
+        gpio_write_pin(screen_drivers[i].cs, 1);
+        gpio_write_pin(screen_drivers[i].dc, 1);
+        gpio_set_output_pushpull(screen_drivers[i].reset);
+        gpio_set_output_pushpull(screen_drivers[i].cs);
+        gpio_set_output_pushpull(screen_drivers[i].dc);
+#ifdef SCREEN_DRIVER_ST7735
+        st7735_init(&screen_drivers[i]);
+#else
+        ssd1357_init(&screen_drivers[i]);
+#endif
     }
 
     // create two buffer on screen size
@@ -89,7 +103,11 @@ void screen_ticks(uint32_t ticks)
 
 void screen_fill(const void* data)
 {
-    ssd1357_fill(&ssd1357_drivers[0], data);
+#ifdef SCREEN_DRIVER_ST7735
+    st7735_fill(&screen_drivers[0], data);
+#else
+    ssd1357_fill(&screen_drivers[0], data);
+#endif
 }
 
 void screen_test(void)
@@ -102,5 +120,9 @@ void screen_test(void)
 
 void screen_fill_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, const void *data, uint32_t data_size)
 {
-    ssd1357_fill_rect(&ssd1357_drivers[0], x, y, width, height, data, data_size);
+#ifdef SCREEN_DRIVER_ST7735
+    st7735_fill_rect(&screen_drivers[0], x, y, width, height, data, data_size);
+#else
+    ssd1357_fill_rect(&screen_drivers[0], x, y, width, height, data, data_size);
+#endif
 }
