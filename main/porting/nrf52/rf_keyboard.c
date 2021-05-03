18e10dd06b03893807317685b3481d3f7b24942f
Lei Yu <yuleiz@gmail.com>
2021-05-03 13:21:34 +0800

    store gzll reset

--- main/porting/nrf52/rf_keyboard.c
+++ main/porting/nrf52/rf_keyboard.c
@@ -422,7 +422,7 @@ bool hook_process_action_main(keyrecord_t *record) {
                 sd_nvic_SystemReset();
             } else {
                 NRF_LOG_INFO("switch to ble");
-                NRF_POWER->GPREGRET = 0;
+                NRF_POWER->GPREGRET &= ~RST_USE_GZLL;
                 NVIC_SystemReset();
             } return true;
         default:
