d4b056c33f1f4c50e88e6ff2bab74b356d7612a3
Lei Yu <yuleiz@gmail.com>
2021-06-24 20:56:27 +0800

    use internal rc

--- sdk_nrf5/sdk_config.h
+++ sdk_nrf5/sdk_config.h
@@ -1726,7 +1726,7 @@
 // <196609=> External Full Swing
 
 #ifndef NRFX_CLOCK_CONFIG_LF_SRC
-#define NRFX_CLOCK_CONFIG_LF_SRC 1
+#define NRFX_CLOCK_CONFIG_LF_SRC 0
 #endif
 
 // <o> NRFX_CLOCK_CONFIG_IRQ_PRIORITY  - Interrupt priority
@@ -4667,7 +4667,7 @@
 // <196609=> External Full Swing
 
 #ifndef CLOCK_CONFIG_LF_SRC
-#define CLOCK_CONFIG_LF_SRC 1
+#define CLOCK_CONFIG_LF_SRC 0
 #endif
 
 // <q> CLOCK_CONFIG_LF_CAL_ENABLED  - Calibration enable for LF Clock Source
