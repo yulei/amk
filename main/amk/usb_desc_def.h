2852d4c0348089ef2fb248848a7702efc4f3d1b5
Lei Yu <yuleiz@gmail.com>
2021-09-29 15:54:16 +0800

    update usb hid macro definitions

--- main/amk/usb_desc_def.h
+++ main/amk/usb_desc_def.h
@@ -18,7 +18,8 @@ enum
     UDD_DESC_ENDPOINT       = 0x05
 };
 
-enum {
+enum
+{
     UDD_ATT_REMOTE_WAKEUP = 1<<5,
     UDD_ATT_SELF_POWERED  = 1<<6,
 };
@@ -32,33 +33,33 @@ enum
 
 enum
 {
-    HID_SUBCLASS_NONE = 0,
-    HID_SUBCLASS_BOOT = 1,
+    UDD_HID_SUBCLASS_NONE = 0,
+    UDD_HID_SUBCLASS_BOOT = 1,
 };
 
 enum
 {
-    HID_DESC_TYPE_HID       = 0x21,
-    HID_DESC_TYPE_REPORT    = 0x22,
-    HID_DESC_TYPE_PHYSICAL  = 0x23,
+    UDD_HID_DESC_TYPE_HID       = 0x21,
+    UDD_HID_DESC_TYPE_REPORT    = 0x22,
+    UDD_HID_DESC_TYPE_PHYSICAL  = 0x23,
 };
 
 enum
 {
-    MSC_SUBCLASS_SCSI   = 0x06,
+    UDD_MSC_SUBCLASS_SCSI = 0x06,
 };
 
 enum
 {
-    MSC_PROTOCOL_BOT    = 0x50,
+    UDD_MSC_PROTOCOL_BOT = 0x50,
 };
 
 enum
 {
-  UDD_XFER_CONTROL      = 0,
-  UDD_XFER_ISOCHRONOUS  = 1,
-  UDD_XFER_BULK         = 2,
-  UDD_XFER_INTERRUPT    = 3,
+    UDD_EP_CONTROL      = 0,
+    UDD_EP_ISOCHRONOUS  = 1,
+    UDD_EP_BULK         = 2,
+    UDD_EP_INTERRUPT    = 3,
 };
 
 
@@ -78,44 +79,44 @@ enum
     9, UDD_DESC_CONFIGURATION, UDD_LOW(total), UDD_HIGH(total), itf_num, config, str, (1<<7) | attr, power/2
 
 #define UDD_HID_DESCRIPTOR(itf, str, boot, report_len, epin, epsize, interval) \
-    9, UDD_DESC_INTERFACE, itf, 0, 1, UDD_CLASS_HID, ((boot) ? HID_SUBCLASS_BOOT : 0), boot, str,\
-    9, HID_DESC_TYPE_HID, 0x11, 0x01, 0, 1, HID_DESC_TYPE_REPORT, UDD_LOW(report_len), UDD_HIGH(report_len),\
-    7, UDD_DESC_ENDPOINT, epin, UDD_XFER_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval 
+    9, UDD_DESC_INTERFACE, itf, 0, 1, UDD_CLASS_HID, ((boot) ? UDD_HID_SUBCLASS_BOOT : 0), boot, str,\
+    9, UDD_HID_DESC_TYPE_HID, 0x11, 0x01, 0, 1, UDD_HID_DESC_TYPE_REPORT, UDD_LOW(report_len), UDD_HIGH(report_len),\
+    7, UDD_DESC_ENDPOINT, epin, UDD_EP_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval 
 
 #define UDD_HID_GENERIC_DESCRIPTOR(itf, str, boot, report_len, epout, epin, epsize, interval) \
-    9, UDD_DESC_INTERFACE, itf, 0, 2, UDD_CLASS_HID, ((boot) ? HID_SUBCLASS_BOOT : 0), boot, str,\
-    9, HID_DESC_TYPE_HID, 0x11, 0x01, 0, 1, HID_DESC_TYPE_REPORT, UDD_LOW(report_len), UDD_HIGH(report_len),\
-    7, UDD_DESC_ENDPOINT, epout, UDD_XFER_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval, \
-    7, UDD_DESC_ENDPOINT, epin, UDD_XFER_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval
+    9, UDD_DESC_INTERFACE, itf, 0, 2, UDD_CLASS_HID, ((boot) ? UDD_HID_SUBCLASS_BOOT : 0), boot, str,\
+    9, UDD_HID_DESC_TYPE_HID, 0x11, 0x01, 0, 1, UDD_HID_DESC_TYPE_REPORT, UDD_LOW(report_len), UDD_HIGH(report_len),\
+    7, UDD_DESC_ENDPOINT, epout, UDD_EP_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval, \
+    7, UDD_DESC_ENDPOINT, epin, UDD_EP_INTERRUPT, UDD_LOW(epsize), UDD_HIGH(epsize), interval
 
 #define UDD_MSC_DESCRIPTOR(itf, str, epout, epin, epsize) \
-    9, UDD_DESC_INTERFACE, itf, 0, 2, UDD_CLASS_MSC, MSC_SUBCLASS_SCSI, MSC_PROTOCOL_BOT, str,\
-    7, UDD_DESC_ENDPOINT, epout, UDD_XFER_BULK, UDD_LOW(epsize), UDD_HIGH(epsize), 0,\
-    7, UDD_DESC_ENDPOINT, epin, UDD_XFER_BULK, UDD_LOW(epsize), UDD_HIGH(epsize), 0
+    9, UDD_DESC_INTERFACE, itf, 0, 2, UDD_CLASS_MSC, UDD_MSC_SUBCLASS_SCSI, UDD_MSC_PROTOCOL_BOT, str,\
+    7, UDD_DESC_ENDPOINT, epout, UDD_EP_BULK, UDD_LOW(epsize), UDD_HIGH(epsize), 0,\
+    7, UDD_DESC_ENDPOINT, epin, UDD_EP_BULK, UDD_LOW(epsize), UDD_HIGH(epsize), 0
 
 #define UDD_CONFIG_DESC_LEN         (9)
 #define UDD_HID_DESC_LEN            (9+9+7)
 #define UDD_HID_GENERIC_DESC_LEN    (9+9+7+7)
 #define UDD_MSC_DESC_LEN            (9+7+7)
 
-#define USAGE_PAGE(x)	        0x05, x
-#define USAGE_PAGE_2(x)	        0x06, UDD_LOW(x), UDD_HIGH(x)
-#define USAGE(x)	            0x09, x
-#define USAGE_2(x)	            0x0A, UDD_LOW(x), UDD_HIGH(x)
-#define COLLECTION(x)           0xA1, x
-#define USAGE_MINIMUM(x)        0x19, x
-#define USAGE_MAXIMUM(x)        0x29, x
-#define USAGE_MAXIMUM_2(x)      0x2A, UDD_LOW(x), UDD_HIGH(x)
-#define LOGICAL_MINIMUM(x)	    0x15, x
-#define LOGICAL_MAXIMUM(x)      0x25, x
-#define LOGICAL_MAXIMUM_2(x)    0x26, UDD_LOW(x), UDD_HIGH(x)
-#define REPORT_COUNT(x)         0x95, x
-#define REPORT_SIZE(x)          0x75, x
-#define REPORT_ID(x)            0x85, x
-#define KBD_REPORT_ID(x)        0x85, x,
-#define INPUT(x)                0x81, (x)
-#define OUTPUT(x)               0x91, (x)
-#define END_COLLECTION(x)       0xC0
+#define UDD_USAGE_PAGE(x)	        0x05, x
+#define UDD_USAGE_PAGE_2(x)	        0x06, UDD_LOW(x), UDD_HIGH(x)
+#define UDD_USAGE(x)	            0x09, x
+#define UDD_USAGE_2(x)	            0x0A, UDD_LOW(x), UDD_HIGH(x)
+#define UDD_COLLECTION(x)           0xA1, x
+#define UDD_USAGE_MINIMUM(x)        0x19, x
+#define UDD_USAGE_MAXIMUM(x)        0x29, x
+#define UDD_USAGE_MAXIMUM_2(x)      0x2A, UDD_LOW(x), UDD_HIGH(x)
+#define UDD_LOGICAL_MINIMUM(x)	    0x15, x
+#define UDD_LOGICAL_MAXIMUM(x)      0x25, x
+#define UDD_LOGICAL_MAXIMUM_2(x)    0x26, UDD_LOW(x), UDD_HIGH(x)
+#define UDD_REPORT_COUNT(x)         0x95, x
+#define UDD_REPORT_SIZE(x)          0x75, x
+#define UDD_REPORT_ID(x)            0x85, x
+//#define UDD_KBD_REPORT_ID(x)        0x85, x,
+#define UDD_INPUT(x)                0x81, (x)
+#define UDD_OUTPUT(x)               0x91, (x)
+#define UDD_END_COLLECTION(x)       0xC0
 
 #define UDD_HID_DATA            (0<<0)
 #define UDD_HID_CONSTANT        (1<<0)
@@ -125,153 +126,153 @@ enum
 #define UDD_HID_RELATIVE        (1<<2)
 
 #define UDD_HID_REPORT_DESC_KEYBOARD(...) \
-    USAGE_PAGE(0x01), \
-    USAGE(0x06), \
-    COLLECTION(0x01), \
+    UDD_USAGE_PAGE(0x01), \
+    UDD_USAGE(0x06), \
+    UDD_COLLECTION(0x01), \
         __VA_ARGS__ \
 \
-        USAGE_PAGE(0x07), \
-        USAGE_MINIMUM(0xE0), \
-        USAGE_MAXIMUM(0xE7), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM(0x01), \
-        REPORT_COUNT(0x08), \
-        REPORT_SIZE(0x01), \
-        INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+        UDD_USAGE_PAGE(0x07), \
+        UDD_USAGE_MINIMUM(0xE0), \
+        UDD_USAGE_MAXIMUM(0xE7), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM(0x01), \
+        UDD_REPORT_COUNT(0x08), \
+        UDD_REPORT_SIZE(0x01), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
 \
-        REPORT_COUNT(0x01), \
-        REPORT_SIZE(0x08), \
-        INPUT(UDD_HID_CONSTANT), \
+        UDD_REPORT_COUNT(0x01), \
+        UDD_REPORT_SIZE(0x08), \
+        UDD_INPUT(UDD_HID_CONSTANT), \
 \
-        USAGE_PAGE(0x07), \
-        USAGE_MINIMUM(0x00), \
-        USAGE_MAXIMUM(0xFF), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM_2(0x00FF), \
-        REPORT_COUNT(0x06), \
-        REPORT_SIZE(0x08), \
-        INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
+        UDD_USAGE_PAGE(0x07), \
+        UDD_USAGE_MINIMUM(0x00), \
+        UDD_USAGE_MAXIMUM(0xFF), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM_2(0x00FF), \
+        UDD_REPORT_COUNT(0x06), \
+        UDD_REPORT_SIZE(0x08), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
 \
-        USAGE_PAGE(0x08), \
-        USAGE_MINIMUM(0x01), \
-        USAGE_MAXIMUM(0x05), \
-        REPORT_COUNT(0x05), \
-        REPORT_SIZE(0x01), \
-        OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
-        REPORT_COUNT(0x01), \
-        REPORT_SIZE(0x03), \
-        OUTPUT(UDD_HID_CONSTANT), \
-    END_COLLECTION(0)
+        UDD_USAGE_PAGE(0x08), \
+        UDD_USAGE_MINIMUM(0x01), \
+        UDD_USAGE_MAXIMUM(0x05), \
+        UDD_REPORT_COUNT(0x05), \
+        UDD_REPORT_SIZE(0x01), \
+        UDD_OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+        UDD_REPORT_COUNT(0x01), \
+        UDD_REPORT_SIZE(0x03), \
+        UDD_OUTPUT(UDD_HID_CONSTANT), \
+    UDD_END_COLLECTION(0)
 
 #define UDD_HID_REPORT_DESC_MOUSE(mouse) \
-    USAGE_PAGE(0x01), \
-    USAGE(0x02), \
-    COLLECTION(0x01), \
-        REPORT_ID(mouse), \
-        USAGE(0x01), \
-        COLLECTION(0x00), \
+    UDD_USAGE_PAGE(0x01), \
+    UDD_USAGE(0x02), \
+    UDD_COLLECTION(0x01), \
+        UDD_REPORT_ID(mouse), \
+        UDD_USAGE(0x01), \
+        UDD_COLLECTION(0x00), \
 \
-            USAGE_PAGE(0x09), \
-            USAGE_MINIMUM(0x01), \
-            USAGE_MAXIMUM(0x08), \
-            LOGICAL_MINIMUM(0x00), \
-            LOGICAL_MAXIMUM(0x01), \
-            REPORT_COUNT(0x08), \
-            REPORT_SIZE(0x01), \
-            INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+            UDD_USAGE_PAGE(0x09), \
+            UDD_USAGE_MINIMUM(0x01), \
+            UDD_USAGE_MAXIMUM(0x08), \
+            UDD_LOGICAL_MINIMUM(0x00), \
+            UDD_LOGICAL_MAXIMUM(0x01), \
+            UDD_REPORT_COUNT(0x08), \
+            UDD_REPORT_SIZE(0x01), \
+            UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
 \
-            USAGE_PAGE(0x01), \
-            USAGE(0x30), \
-            USAGE(0x31), \
-            LOGICAL_MINIMUM(-127), \
-            LOGICAL_MAXIMUM(127), \
-            REPORT_COUNT(0x02), \
-            REPORT_SIZE(0x08), \
-            INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
+            UDD_USAGE_PAGE(0x01), \
+            UDD_USAGE(0x30), \
+            UDD_USAGE(0x31), \
+            UDD_LOGICAL_MINIMUM(-127), \
+            UDD_LOGICAL_MAXIMUM(127), \
+            UDD_REPORT_COUNT(0x02), \
+            UDD_REPORT_SIZE(0x08), \
+            UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
 \
-            USAGE(0x38), \
-            LOGICAL_MINIMUM(-127), \
-            LOGICAL_MAXIMUM(127), \
-            REPORT_COUNT(0x01), \
-            REPORT_SIZE(0x08), \
-            INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
+            UDD_USAGE(0x38), \
+            UDD_LOGICAL_MINIMUM(-127), \
+            UDD_LOGICAL_MAXIMUM(127), \
+            UDD_REPORT_COUNT(0x01), \
+            UDD_REPORT_SIZE(0x08), \
+            UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
 \
-            USAGE_PAGE(0x0C), \
-            USAGE_2(0x0238), \
-            LOGICAL_MINIMUM(-127), \
-            LOGICAL_MAXIMUM(127), \
-            REPORT_COUNT(0x01), \
-            REPORT_SIZE(0x08), \
-            INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
-        END_COLLECTION(0), \
-    END_COLLECTION(0) \
+            UDD_USAGE_PAGE(0x0C), \
+            UDD_USAGE_2(0x0238), \
+            UDD_LOGICAL_MINIMUM(-127), \
+            UDD_LOGICAL_MAXIMUM(127), \
+            UDD_REPORT_COUNT(0x01), \
+            UDD_REPORT_SIZE(0x08), \
+            UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_RELATIVE), \
+        UDD_END_COLLECTION(0), \
+    UDD_END_COLLECTION(0) \
 
 #define UDD_HID_REPORT_DESC_EXTRA(system, comsumer) \
-    USAGE_PAGE(0x01), \
-    USAGE(0x80), \
-    COLLECTION(0x01), \
-        REPORT_ID(system), \
-        USAGE_MINIMUM(0x01), \
-        USAGE_MAXIMUM_2(0x00B7), \
-        LOGICAL_MINIMUM(0x01), \
-        LOGICAL_MAXIMUM_2(0x00B7), \
-        REPORT_COUNT(1), \
-        REPORT_SIZE(16), \
-        INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
-    END_COLLECTION(0), \
+    UDD_USAGE_PAGE(0x01), \
+    UDD_USAGE(0x80), \
+    UDD_COLLECTION(0x01), \
+        UDD_REPORT_ID(system), \
+        UDD_USAGE_MINIMUM(0x01), \
+        UDD_USAGE_MAXIMUM_2(0x00B7), \
+        UDD_LOGICAL_MINIMUM(0x01), \
+        UDD_LOGICAL_MAXIMUM_2(0x00B7), \
+        UDD_REPORT_COUNT(1), \
+        UDD_REPORT_SIZE(16), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
+    UDD_END_COLLECTION(0), \
 \
-    USAGE_PAGE(0x0C), \
-    USAGE(0x01), \
-    COLLECTION(0x01), \
-        REPORT_ID(comsumer), \
-        USAGE_MINIMUM(0x01), \
-        USAGE_MAXIMUM_2(0x02A0), \
-        LOGICAL_MINIMUM(0x01), \
-        LOGICAL_MAXIMUM_2(0x02A0), \
-        REPORT_COUNT(1), \
-        REPORT_SIZE(16), \
-        INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
-    END_COLLECTION(0)
+    UDD_USAGE_PAGE(0x0C), \
+    UDD_USAGE(0x01), \
+    UDD_COLLECTION(0x01), \
+        UDD_REPORT_ID(comsumer), \
+        UDD_USAGE_MINIMUM(0x01), \
+        UDD_USAGE_MAXIMUM_2(0x02A0), \
+        UDD_LOGICAL_MINIMUM(0x01), \
+        UDD_LOGICAL_MAXIMUM_2(0x02A0), \
+        UDD_REPORT_COUNT(1), \
+        UDD_REPORT_SIZE(16), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_ARRAY | UDD_HID_ABSOLUTE), \
+    UDD_END_COLLECTION(0)
 
 #ifndef  REPORT_BITS
 #define REPORT_BITS 30
 #endif
 
 #define UDD_HID_REPORT_DESC_NKRO(nkro) \
-    USAGE_PAGE(0x01), \
-    USAGE(0x06), \
-    COLLECTION(0x01), \
+    UDD_USAGE_PAGE(0x01), \
+    UDD_USAGE(0x06), \
+    UDD_COLLECTION(0x01), \
 \
-        REPORT_ID(nkro), \
-        USAGE_PAGE(0x07), \
-        USAGE_MINIMUM(0xE0), \
-        USAGE_MAXIMUM(0xE7), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM(0x01), \
-        REPORT_COUNT(0x08), \
-        REPORT_SIZE(0x01), \
-        INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+        UDD_REPORT_ID(nkro), \
+        UDD_USAGE_PAGE(0x07), \
+        UDD_USAGE_MINIMUM(0xE0), \
+        UDD_USAGE_MAXIMUM(0xE7), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM(0x01), \
+        UDD_REPORT_COUNT(0x08), \
+        UDD_REPORT_SIZE(0x01), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
 \
-        USAGE_PAGE(0x07), \
-        USAGE_MINIMUM(0x00), \
-        USAGE_MAXIMUM(REPORT_BITS * 8 - 1), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM(0x01), \
-        REPORT_COUNT(REPORT_BITS * 8), \
-        REPORT_SIZE(0x01), \
-        INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+        UDD_USAGE_PAGE(0x07), \
+        UDD_USAGE_MINIMUM(0x00), \
+        UDD_USAGE_MAXIMUM(REPORT_BITS * 8 - 1), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM(0x01), \
+        UDD_REPORT_COUNT(REPORT_BITS * 8), \
+        UDD_REPORT_SIZE(0x01), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
 \
-        USAGE_PAGE(0x08), \
-        USAGE_MINIMUM(0x01), \
-        USAGE_MAXIMUM(0x05), \
-        REPORT_COUNT(0x05), \
-        REPORT_SIZE(0x01), \
-        OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE | UDD_HID_NON_VOLATILE), \
+        UDD_USAGE_PAGE(0x08), \
+        UDD_USAGE_MINIMUM(0x01), \
+        UDD_USAGE_MAXIMUM(0x05), \
+        UDD_REPORT_COUNT(0x05), \
+        UDD_REPORT_SIZE(0x01), \
+        UDD_OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE | UDD_HID_NON_VOLATILE), \
 \
-        REPORT_COUNT(0x01), \
-        REPORT_SIZE(0x03), \
-        OUTPUT(UDD_HID_CONSTANT), \
-    END_COLLECTION(0)
+        UDD_REPORT_COUNT(0x01), \
+        UDD_REPORT_SIZE(0x03), \
+        UDD_OUTPUT(UDD_HID_CONSTANT), \
+    UDD_END_COLLECTION(0)
 
 #ifndef RAW_USAGE_PAGE
 #    define RAW_USAGE_PAGE 0xFF60
@@ -282,20 +283,20 @@ enum
 #endif
 
 #define UDD_HID_REPORT_DESC_VIAL(page, id, size) \
-    USAGE_PAGE_2(page), \
-    USAGE(id), \
-    COLLECTION(0x01), \
-        USAGE(0x62), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM_2(0x00FF), \
-        REPORT_COUNT(size), \
-        REPORT_SIZE(0x08), \
-        INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
+    UDD_USAGE_PAGE_2(page), \
+    UDD_USAGE(id), \
+    UDD_COLLECTION(0x01), \
+        UDD_USAGE(0x62), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM_2(0x00FF), \
+        UDD_REPORT_COUNT(size), \
+        UDD_REPORT_SIZE(0x08), \
+        UDD_INPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE), \
 \
-        USAGE(0x63), \
-        LOGICAL_MINIMUM(0x00), \
-        LOGICAL_MAXIMUM_2(0x00FF), \
-        REPORT_COUNT(RAW_EPSIZE), \
-        REPORT_SIZE(0x08), \
-        OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE | UDD_HID_NON_VOLATILE), \
-    END_COLLECTION(0)
+        UDD_USAGE(0x63), \
+        UDD_LOGICAL_MINIMUM(0x00), \
+        UDD_LOGICAL_MAXIMUM_2(0x00FF), \
+        UDD_REPORT_COUNT(RAW_EPSIZE), \
+        UDD_REPORT_SIZE(0x08), \
+        UDD_OUTPUT(UDD_HID_DATA | UDD_HID_VARIABLE | UDD_HID_ABSOLUTE | UDD_HID_NON_VOLATILE), \
+    UDD_END_COLLECTION(0)
