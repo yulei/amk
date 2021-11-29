49a517776b337c08d46ec2c90180b204ecfa22ad
Lei Yu <yuleiz@gmail.com>
2021-11-29 20:42:46 +0800

    update vita classic keymap

--- keyboards/m65_v2/m65.c
+++ keyboards/m65_v2/m65.c
@@ -284,15 +284,16 @@ rgb_led_t g_rgb_leds[RGB_LED_NUM] = {
     {0, 1, 1, 1},
     {0, 2, 2, 2},
     {0, 3, 3, 3},
+    {0, 4, 4, 4},
 };
 
 
 rgb_device_t g_rgb_devices[RGB_DEVICE_NUM] = {
-    {RGB_DRIVER_WS2812, 0, 0, 0, 4},
+    {RGB_DRIVER_WS2812, 0, 0, 1, 4},
 };
 
 rgb_param_t g_rgb_linear_params[RGB_SEGMENT_NUM] = {
-    {0, 0, 4},
+    {0, 1, 4},
 };
 #endif
 
