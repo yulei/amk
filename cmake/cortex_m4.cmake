9ca0e95bcbb0754336c59031cced3438776b66a4
Lei Yu <yuleiz@gmail.com>
2022-08-24 20:26:27 +0800

    update cmsis to 5.9

--- cmake/cortex_m4.cmake
+++ cmake/cortex_m4.cmake
@@ -2,5 +2,3 @@
 set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
 
 set(MCU_FLAGS -mcpu=cortex-m4 -mthumb -mabi=aapcs -mfpu=fpv4-sp-d16 -mfloat-abi=hard CACHE STRING INTERNAL)
-
-include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
