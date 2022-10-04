d3e7be400636972c7288a2c1a747952bef3f659f
Lei Yu <yuleiz@gmail.com>
2022-10-04 15:43:27 +0800

    added spi support for stm32f446

--- main/drivers/spi.c
+++ main/drivers/spi.c
@@ -4,7 +4,7 @@
 
 #if defined(NRF52832_XXAA) || defined(NRF52840_XXAA)
 #   include "spi_nrf52.c"
-#elif defined(STM32F411xE) || defined(STM32F722xx) || defined(STM32F405xx) || defined(STM32F401xC)
+#elif defined(STM32F411xE) || defined(STM32F722xx) || defined(STM32F405xx) || defined(STM32F401xC) || defined(STM32F446xx)
 #   include "spi_stm32.c"
 #elif defined(HC32F460)
 #   include "spi_hc32.c"
