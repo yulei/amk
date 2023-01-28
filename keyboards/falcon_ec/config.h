a74db6f7828fab90d7d7769e5dbdbf06ea3428a4
Lei Yu <yuleiz@gmail.com>
2023-01-28 11:33:01 +0800

    update falcon ec vial support

--- keyboards/falcon_ec/config.h
+++ keyboards/falcon_ec/config.h
@@ -15,19 +15,19 @@
 #define L_MASK          0x08
 #define R_MASK          0x10
 
-#define MATRIX_ROW_PINS {ROW_1_PIN, ROW_2_PIN, ROW_3_PIN, ROW_4_PIN}
-#define MATRIX_COL_PINS {L_MASK|3, L_MASK|0, L_MASK|1, L_MASK|2, L_MASK|4, L_MASK|6, L_MASK|7, L_MASK|5, R_MASK|3, R_MASK|0, R_MASK|1, R_MASK|2, R_MASK|4, R_MASK|6, R_MASK|7};
-
-#define DISCHARGE_WAIT_PRE  150
-#define DISCHARGE_WAIT_POST 100
-//#define STROKE_DEFAULT  16
-#define CHARGE_WAIT     2
-//define EC_SENSE_MAX    2560
-
 #define ROW_1_PIN       B13
 #define ROW_2_PIN       B14
 #define ROW_3_PIN       B15
 #define ROW_4_PIN       B12
+
+#define MATRIX_ROW_PINS {ROW_1_PIN, ROW_2_PIN, ROW_3_PIN, ROW_4_PIN}
+#define MATRIX_COL_PINS {L_MASK|3, L_MASK|0, L_MASK|1, L_MASK|2, L_MASK|4, L_MASK|6, L_MASK|7, L_MASK|5, R_MASK|3, R_MASK|0, R_MASK|1, R_MASK|2, R_MASK|4, R_MASK|6, R_MASK|7};
+
+#define DISCHARGE_WAIT_PRE  30
+#define DISCHARGE_WAIT_POST 30
+#define CHARGE_WAIT     2
+#define EC_TH_LOW       1024
+#define EC_TH_HIGH      1280
 
 #define LEFT_EN_PIN     A7
 #define RIGHT_EN_PIN    A8
@@ -43,7 +43,7 @@
 #define KEY_IN_CHANNEL  ADC_CHANNEL_2
 #define KEY_IN_PIN      GPIO_PIN_2
 #define KEY_IN_PORT     GPIOA
-#define SENSE_TH        1192
+#define SENSE_TH        1192//EC_TH_HIGH
 
 #define CAPS_LED_PIN    A6
 
@@ -54,14 +54,21 @@
 #define PWM_TIM_PERIOD      119
 #define PWM_TIM             htim4
 #define PWM_TIM_CHANNEL     TIM_CHANNEL_2
+#define PWM_TIM_DMA_ID      TIM_DMA_ID_CC2
+
+#define PWM_DMA_INSTANCE    DMA1_Stream3
+#define PWM_DMA_CHANNEL     DMA_CHANNEL_2
+#define PWM_DMA_IRQHnadler  DMA1_Stream3_IRQHandler
+#define PWM_PIN             GPIO_PIN_7
 
 #define RGB_LED_NUM     4
 #define RGB_DEVICE_NUM  1
 #define RGB_SEGMENT_NUM 1
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE        2
+#define DEBOUNCE        10
 #define DWT_DELAY
+#define SUSPEND_RECONNECT
 
 #define VECT_TAB_OFFSET 0x20000
 #define HSE_VALUE       16000000
--- keyboards/falcon_ec/falcon_ec.mk
+++ keyboards/falcon_ec/falcon_ec.mk
@@ -6,8 +6,9 @@ TINYUSB_ENABLE = yes
 TINYUSB_USE_HAL = yes
 RGB_LINEAR_ENABLE = yes
 EECONFIG_FLASH = yes
-MATRIX_EC_ENABLE = yes
-#VIAL_ENABLE = yes
+#MATRIX_EC_ENABLE = yes
+VIAL_ENABLE = yes
+NKRO_AUTO_ENABLE = yes
 
 LINKER_PATH = $(KEYBOARD_DIR)
 
@@ -15,7 +16,9 @@ UF2_ENABLE = yes
 UF2_FAMILY = STM32F4
 
 SRCS += $(KEYBOARD_DIR)/falcon_ec.c
-SRCS += $(KEYBOARD_DIR)/falcon_ec_keymap.c
+SRCS += $(KEYBOARD_DIR)/falcon_ec_gcmap.c
+SRCS += $(KEYBOARD_DIR)/ec_matrix.c
+#SRCS += $(KEYBOARD_DIR)/falcon_ec_keymap.c
 #SRCS += $(KEYBOARD_DIR)/falcon_ec_actionmap.c
 
 #SRCS += $(KEYBOARD_DIR)/custom_matrix.c
