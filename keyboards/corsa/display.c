0d633426552318c07fe787eabc280e2d919dab3d
Lei Yu <yuleiz@gmail.com>
2024-04-22 21:33:44 +0800

    added flash storage cleanup

--- keyboards/corsa/display.c
+++ keyboards/corsa/display.c
@@ -416,10 +416,10 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record)
             render = &renders[0];
             render->mode = (render->mode+1) % MODE_MAX;
         } return false;
-        //case KC_F23:
-        //    msc_erase();
-        //    reset_to_msc(true);
-        //    return false;
+        case KC_F23:
+            msc_erase();
+            reset_to_msc(true);
+            return false;
         case KC_F24: 
             reset_to_msc((usb_setting & USB_MSC_BIT) ? false : true);
             return false;
